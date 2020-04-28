6da4904882b2f055cd00aae49aed07e79c1c92ff
Mohammed J Hossain <mjhossainnyc@gmail.com>
2020-04-28 01:46:03 -0400

    Midterm Project Initiated

---
+++ home.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>City Flex</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-3 menuBar">
+          <br><br>
+          <div class="row justify-content-center p-2">
+
+            <h3>City Flex</h3>
+          </div>
+          <br><br>
+          <div class="col-md-12 menu"></div>
+        </div>
+        <div class="col-md-9 dashboardBody">
+          <div class="row mt-2 topBar p-2">
+            <div class="col-md-3">
+              <h4 class="align-center">Dashboard</h4>
+            </div>
+          </div>
+          <div class="row p-3">
+
+              <div class="col-md-3 shadow card1">
+                <div class="col-md-12"></div>
+              </div>
+              <div class="col-md-3 shadow card2">
+                <div class="col-md-12"></div>
+              </div>
+              <div class="col-md-4 shadow card3">
+                <div class="col-md-12"></div>
+              </div>
+
+          </div>
+          <div class="row justify-content-center mt-5">
+            <h4 class="text-center">Members</h4>
+          </div>
+          <div class="row justify-content-center mt-2">
+
+            <div class="col-md-10 p-2">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+
+                </tbody>
+              </table>
+              <a href="" class="btn-danger btn-lg btn-block text-center">Manage Members</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>City Flex</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <div class="mx-auto mainBG">
+      <div class="row justify-content-center align-items-center bodyWrapper">
+        <div class="shadow col-md-4 offset-md-4 p-5 loginBox justify-content-center">
+          <h3 class="text-center">City Flex: Admin Dashboard</h3>
+          <form class="" action="index.html" method="post">
+            <input type="text" id="username" name="username" class="form-control formField mt-5" placeholder="Username">
+            <br>
+            <input type="password" id="password" name="password" class="form-control formField mb-4" placeholder="Password">
+            <button type="button" name="submit" class="btn-primary btn-lg btn-block mb-5">Login</button>
+          </form>
+          <p class="text-center">Don't have an accout?</p>
+          <hr>
+          <button type="button" name="register" class="btn-warning btn-lg btn-block">Register</button>
+        </div>
+      </div>
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
